f1ab4348efbb9cd57772bb6a51af8275a27064a2
punithnr1 <punithnr48@gmail.com>
2024-07-22 19:25:10 +0530

    Add files via upload
    final project

--- compliance-rule-engine/src/test/java/com/team4/compliance/ComplianceRuleEngineApplicationTests.java
+++ compliance-rule-engine/src/test/java/com/team4/compliance/ComplianceRuleEngineApplicationTests.java
@@ -1,11 +1,13 @@
 package com.team4.compliance;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
+
 public class ComplianceRuleEngineApplicationTests {
+
     @Test
     public void testAnalyzeCompliance() {
         ComplianceRule rule = new ComplianceRule("rule1", "Test Rule", config -> config.getDeviceId().equals("device1"));
